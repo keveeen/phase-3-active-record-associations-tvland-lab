11be6a6b4df976a114d49606c4e38d1b0f2fbb33
keveeen <105822487+keveeen@users.noreply.github.com>
2022-11-18 18:25:04 +0100

    commit tree 802711de2780973ad242144d2914ead5e1aab74b

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,26 @@
 class Actor < ActiveRecord::Base
-  
+
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def characters
+
+        self.characters
+        
+
+    end
+
+    def show
+
+        self.shows
+
+    end
+
+    def say_that_thing_you_say
+
+        
+
+    end
+
+    
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,15 @@
 class Character < ActiveRecord::Base
+    belongs_to :actor
+    belongs_to :show
+
+
+    def actor
+        self.actor
+
+    end
   
+def show
+    self.show
+end
+
 end
